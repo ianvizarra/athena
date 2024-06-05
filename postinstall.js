154ec50f56376ff2aeb2cc2edc58aa1427d623ab
ianvizarra <ian.vizarra@gmail.com>
2024-06-05 11:34:11 +0800

    chore: add post install message

---
+++ postinstall.js
@@ -0,0 +1,20 @@
+console.log('\x1b[31m%s\x1b[0m', '\nWelcome to Athena.VC')
+console.log(
+  '\x1b[36m%s\x1b[0m',
+  "\nWe're helping 20 coders launch their first company with 12 months of mentorship from successful entrepreneurs and $10,000-$50,000 USD in funding\n",
+)
+console.log('\x1b[31m%s\x1b[0m', 'No startup experience required\n')
+
+console.log(
+  '\x1b[36m%s\x1b[0m',
+  "You'll learn step by step how to build an 8 figure business from zero. We'll guide you on finding co-founders, ideation, product, sales, marketing + more. You can participate in the program whilst studying or working.",
+)
+console.log('\x1b[31m%s\x1b[0m', '\nStartups are not risky\n')
+console.log(
+  '\x1b[36m%s\x1b[0m',
+  "Everyone we know who's worked 10 years on building startups is making at least $500,000 USD in profit. Startups fail because people quit. " +
+    "It takes 10 years to be a doctor, 10 years to be a lawyer, 10+ years to get good at any anything. It's the same with being a founder, but your upside is uncapped.\n",
+)
+console.log('\x1b[33mApply today at \x1b[31m\x1b]8;;https://www.athena.vc\x1b\\www.athena.vc\x1b]8;;\x1b\\\x1b[0m')
+
+console.log('\x1b[37m%s\x1b[33m%s\x1b[0m', '\n> run ', 'athena start')
